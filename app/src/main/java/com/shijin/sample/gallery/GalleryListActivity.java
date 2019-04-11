98289ac6c7fac37c77283466ce771c3cb62c3af6
shijin <shijinpaleri@gmail.com>
2019-04-11 10:31:47 +0530

    Created a sample Android repo

---
+++ app/src/main/java/com/shijin/sample/gallery/GalleryListActivity.java
@@ -0,0 +1,15 @@
+package com.shijin.sample.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shijin.sample.R;
+
+public class GalleryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_list);
+    }
+}
